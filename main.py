1e5f9dc33f3efd4ff5496b1fd363ae9637fe4ba9
Gautham Kumar <heygautham@gmail.com>
2017-05-16 19:16:09 +0530

    iterates through and sets lyrics using eyed3

--- main.py
+++ main.py
@@ -2,37 +2,59 @@ from bs4 import BeautifulSoup
 import urllib, requests, json, os, eyed3
 
 flag=False
-band=raw_input('Enter band name : ')
-song=raw_input('Enter song name : ')
 searchEngineID='013005384117311148169%3A7rehj54nzye'
 APIKey='AIzaSyAcBWajtXvUDXlv08CtFr7mJamWugAcxmM'
 print
 
-band=band.lower().replace(' ','-')
-song=song.lower().replace(' ','-')
-
-searchUrl='https://www.googleapis.com/customsearch/v1?q='+band+'+'+song+'&cx='+searchEngineID+'&num=1&key='+APIKey
-a = requests.get(searchUrl)
-b = json.loads(a.text)
-
-if b.has_key('items'):
-    url = b['items'][0]['link']
-
-    mainPage=urllib.urlopen(url).read()
-    soup = BeautifulSoup(mainPage, 'html.parser')
-
-    for div in soup.find_all('div'):
-        if div.has_attr('id'):
-            if div['id']=='lyrics-body-text':
-                flag=True
-                mainDiv=div
-                for para in mainDiv.find_all('p'):
-                    print para.text
-                    print
+for subdir, dir, files in os.walk(os.getcwd()):
+    for fileName in files:
+        if fileName.endswith(".mp3"):
+            path=subdir+'/'+fileName
+            try:
+                audio = eyed3.load(path)
+            except ValueError:
+                print "Encoding problem with "+fileName
                 break
 
-    if flag==False:
-        print "Couldn't find lyrics for "+band+" - "+song
-        
-else:
-    print "Couldn't find lyrics for "+band+" - "+song
+            band = audio.tag.artist
+            song = audio.tag.title
+            tempBand = band
+            tempSong = song
+            lyrics=""
+            band=band.lower().replace(' ','-')
+            song=song.lower().replace(' ','-')
+
+            print "Getting lyrics for "+tempBand+" - "+tempSong
+
+            searchUrl='https://www.googleapis.com/customsearch/v1?q='+band+'+'+song+'&cx='+searchEngineID+'&num=1&key='+APIKey
+            a = requests.get(searchUrl)
+            b = json.loads(a.text)
+
+            if b.has_key('items'):
+                url = b['items'][0]['link']
+
+                mainPage=urllib.urlopen(url).read()
+                soup = BeautifulSoup(mainPage, 'html.parser')
+
+                for div in soup.find_all('div'):
+                    if div.has_attr('id'):
+                        if div['id']=='lyrics-body-text':
+                            flag=True
+                            mainDiv=div
+                            for para in mainDiv.find_all('p'):
+                                lyrics+=para.text
+                                lyrics+='\r\r'
+                            break
+
+                if flag==False:
+                    print "Couldn't find lyrics for "+tempBand+" - "+tempSong
+                else:
+                    lyrics = lyrics[:len(lyrics)-2]
+                    audio.tag.lyrics.set(lyrics)
+                    try:
+                        audio.tag.save()
+                    except NotImplementedError:
+                        print "Error - ID3v2.2 for "+tempBand+" - "+tempSong
+                    
+            else:
+                print "Couldn't find lyrics for "+tempBand+" - "+tempSong
