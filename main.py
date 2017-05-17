564100b6a14938847ddd090b7b43759d487a8930
Gautham Kumar <heygautham@gmail.com>
2017-05-17 15:16:28 +0530

    Checks if lyrics already exists. Also checks for artist and song name

--- main.py
+++ main.py
@@ -14,46 +14,59 @@ for subdir, dir, files in os.walk(os.getcwd()):
             except ValueError:
                 print "Encoding problem with "+fileName
                 break
+            
+            try:
+                band = audio.tag.artist
+                song = audio.tag.title
+            except AttributeError:
+                print 'Artist/Song information missing for '+fileName
+                continue
 
-            band = audio.tag.artist
-            song = audio.tag.title
             tempBand = band
             tempSong = song
-            lyrics=""
             band=band.lower().replace(' ','-')
             song=song.lower().replace(' ','-')
 
-            print "Getting lyrics for "+tempBand+" - "+tempSong
+            try:
+                existingLyrics = audio.tag.lyrics[0]
+                print "Lyrics already exist for "+tempBand+" - "+tempSong
+                continue
 
-            searchUrl='https://www.googleapis.com/customsearch/v1?q='+band+'+'+song+'&cx='+searchEngineID+'&num=1&key='+APIKey
-            a = requests.get(searchUrl)
-            b = json.loads(a.text)
+            except IndexError:
+                #Lyrics don't exist. Hence finding it.
+                lyrics=u""
 
-            if b.has_key('items'):
-                url = b['items'][0]['link']
+                print "Getting lyrics for "+tempBand+" - "+tempSong
 
-                mainPage=urllib.urlopen(url).read()
-                soup = BeautifulSoup(mainPage, 'html.parser')
+                searchUrl='https://www.googleapis.com/customsearch/v1?q='+band+'+'+song+'&cx='+searchEngineID+'&num=1&key='+APIKey
+                a = requests.get(searchUrl)
+                b = json.loads(a.text)
 
-                for div in soup.find_all('div'):
-                    if div.has_attr('id'):
-                        if div['id']=='lyrics-body-text':
-                            flag=True
-                            mainDiv=div
-                            for para in mainDiv.find_all('p'):
-                                lyrics+=para.text
-                                lyrics+='\r\r'
-                            break
+                if b.has_key('items'):
+                    url = b['items'][0]['link']
 
-                if flag==False:
-                    print "Couldn't find lyrics for "+tempBand+" - "+tempSong
+                    mainPage=urllib.urlopen(url).read()
+                    soup = BeautifulSoup(mainPage, 'html.parser')
+
+                    for div in soup.find_all('div'):
+                        if div.has_attr('id'):
+                            if div['id']=='lyrics-body-text':
+                                flag=True
+                                mainDiv=div
+                                for para in mainDiv.find_all('p'):
+                                    lyrics+=para.text
+                                    lyrics+='\r\r'
+                                break
+
+                    if flag==False:
+                        print "Couldn't find lyrics for "+tempBand+" - "+tempSong+"\n"
+                    else:
+                        lyrics = lyrics[:len(lyrics)-2]
+                        audio.tag.lyrics.set(lyrics)
+                        try:
+                            audio.tag.save()
+                        except NotImplementedError:
+                            print "Error - ID3v2.2 for "+tempBand+" - "+tempSong+"\n"
+                        
                 else:
-                    lyrics = lyrics[:len(lyrics)-2]
-                    audio.tag.lyrics.set(lyrics)
-                    try:
-                        audio.tag.save()
-                    except NotImplementedError:
-                        print "Error - ID3v2.2 for "+tempBand+" - "+tempSong
-                    
-            else:
-                print "Couldn't find lyrics for "+tempBand+" - "+tempSong
+                    print "Couldn't find lyrics for "+tempBand+" - "+tempSong+"\n"
