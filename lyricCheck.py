9864d5eefa2b6c17c494cffb72e62eadfbf05b96
Gautham Kumar <heygautham@gmail.com>
2017-05-18 19:25:40 +0530

    program to check mp3s which have lyrics already and sort them

---
+++ lyricCheck.py
@@ -0,0 +1,24 @@
+import os, eyed3
+dict={}
+eyed3.log.setLevel("ERROR") #only print errors, not wornings
+for subdir, dir, files in os.walk(os.getcwd()):
+    for fileName in files:
+        if fileName.endswith(".mp3"):
+            try:
+                audio = eyed3.load(subdir+'/'+fileName)
+                band = audio.tag.artist
+                song = audio.tag.title
+                existingLyrics = audio.tag.lyrics[0]
+                if band in dict.keys():
+                    dict[band].append(song)
+                else:
+                    dict[band]=[song]
+            except ValueError:
+                continue
+            except AttributeError:
+                continue
+            except IndexError:
+                continue
+
+for item in dict.keys():
+    print dict[item]
