d9454e8e82cd9b9d2171683f3d0f8e29388ddd0a
Gautham Kumar <heygautham@gmail.com>
2017-08-05 10:02:45 +0530

    Added AZLyrics option

--- main.py
+++ main.py
@@ -12,27 +12,29 @@ for subdir, dir, files in os.walk(os.getcwd()):
     for fileName in files:
         if fileName.endswith(".mp3"):
             path=subdir+'/'+fileName
-            songList.append(path)
+            songList.append(path) #songList contains path of all songs
+
+choice = raw_input("Enter \n1. Google API & MetroLyrics\n2. AZ Lyrics\n")
 
 for path in songList:
     try:
         audio = eyed3.load(path)
     except ValueError:
-        #print "Encoding problem with "+path
+        print "Encoding problem with "+path
         continue
 
     try:
         band = audio.tag.artist
         if band in forbiddenArtists:
-            continue
+            continue #skip song if artist is in forbiddenArtists
             
         song = audio.tag.title
         tempBand = band
         tempSong = song
-        band=band.lower().replace(' ','-')
-        song=song.lower().replace(' ','-')
+        band=band.lower().replace(' ','-') #for query purposes
+        song=song.lower().replace(' ','-') #for query purposes
     except AttributeError:
-        #print 'Artist/Song information missing for '+path
+        print 'Artist/Song information missing for '+path
         continue
 
     try:
@@ -46,36 +48,81 @@ for path in songList:
 
         print "Getting lyrics for "+tempBand+" - "+tempSong
 
-        searchUrl='https://www.googleapis.com/customsearch/v1?q='+band+'+'+song+'&cx='+searchEngineID+'&num=1&key='+APIKey
-        a = requests.get(searchUrl)
-        b = json.loads(a.text)
+        #Google API + Metrolyrics
+        if choice=='1':
+            searchUrl='https://www.googleapis.com/customsearch/v1?q='+band+'+'+song+'&cx='+searchEngineID+'&num=1&key='+APIKey
+            a = requests.get(searchUrl)
+            b = json.loads(a.text)
 
-        if b.has_key('items'):
-            url = b['items'][0]['link']
+            if b.has_key('items'):
+                url = b['items'][0]['link']
 
-            mainPage=urllib.urlopen(url).read()
+                mainPage=urllib.urlopen(url).read()
+                soup = BeautifulSoup(mainPage, 'html.parser')
+
+                for div in soup.find_all('div'):
+                    if div.has_attr('id'):
+                        if div['id']=='lyrics-body-text':
+                            flag=True
+                            mainDiv=div
+                            for para in mainDiv.find_all('p'):
+                                lyrics+=para.text
+                                lyrics+='\r\r'
+                            break
+
+                if flag==False:
+                    print "Couldn't find lyrics for "+tempBand+" - "+tempSong+"\n"
+                else:
+                    lyrics = lyrics[:len(lyrics)-2]
+                    audio.tag.lyrics.set(lyrics)
+                    try:
+                        audio.tag.save()
+                        print "Found lyrics for "+tempBand+" - "+tempSong+"\n"
+                    except NotImplementedError:
+                        print "Error - ID3v2.2 for "+tempBand+" - "+tempSong+"\n"
+                    
+            else:
+                print "Couldn't find lyrics for "+tempBand+" - "+tempSong+"\n"
+
+        #AZLyrics
+        elif choice=='2':
+            searchUrl='http://search.azlyrics.com/search.php?q='+band+'+'+song
+            searchPage = urllib.urlopen(searchUrl).read()
+            searchSoup = BeautifulSoup(searchPage, 'html.parser')
+
+            try:
+                alltds = searchSoup.find_all('td')
+                url = alltds[0].a['href'] #Will invoke Index Error if problem
+                for td in alltds:
+                    if td.has_attr('class'):
+                        url = td.a['href']
+                        break
+            except IndexError:
+                print "Couldn't find that song"
+                continue
+
+            mainPage = urllib.urlopen(url).read()
             soup = BeautifulSoup(mainPage, 'html.parser')
 
+            #Finding div that contains the div that contains the lyrics
             for div in soup.find_all('div'):
-                if div.has_attr('id'):
-                    if div['id']=='lyrics-body-text':
-                        flag=True
-                        mainDiv=div
-                        for para in mainDiv.find_all('p'):
-                            lyrics+=para.text
-                            lyrics+='\r\r'
+                if div.has_attr('class'):
+                    if [u'col-xs-12', u'col-lg-8', u'text-center'] == div['class']:
+                        mainDiv = div
+                        for div in mainDiv.find_all('div'):
+                            if not div.has_attr('class'):
+                                lyrics = div.text
+                                for char in lyrics:
+                                    if (char=='\n' or char=='\r'):
+                                        lyrics=lyrics[1:]
+                                    else:
+                                        break
                         break
 
-            if flag==False:
-                print "Couldn't find lyrics for "+tempBand+" - "+tempSong+"\n"
-            else:
-                lyrics = lyrics[:len(lyrics)-2]
-                audio.tag.lyrics.set(lyrics)
-                try:
-                    audio.tag.save()
-                    print "Found lyrics for "+tempBand+" - "+tempSong+"\n"
-                except NotImplementedError:
-                    print "Error - ID3v2.2 for "+tempBand+" - "+tempSong+"\n"
-                
-        else:
-            print "Couldn't find lyrics for "+tempBand+" - "+tempSong+"\n"
+            #Saves lyrics in mp3 metadata
+            audio.tag.lyrics.set(lyrics)
+            try:
+                audio.tag.save()
+                print "Set lyrics for "+tempBand+" - "+tempSong+"\n"
+            except NotImplementedError:
+                print "Error - ID3v2.2 for "+tempBand+" - "+tempSong+"\n"
